57340af8ed8ac0d8e7f40f2abc5a37edfdb1c97e
rahul-7906 <rahulmelkani4281@gmail.com>
2026-07-02 08:43:09 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,21 @@
+// To print
+console.log("Hello World");
+console.log("My name is Rahul");
+console.log("I am learning javascript");
+
+//Variables
+/* we dont need to declare datatype before usijg it in javascript(Dynamically typed language) */
+name = "Rahul Melkani";
+console.log(name);
+
+age = 17;
+console.log(age);
+
+x = null;
+console.log(x);
+
+y = undefined;
+console.log(y);
+
+isFollow = true;
+console.log(isFollow);
