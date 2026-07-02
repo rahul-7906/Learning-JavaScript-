de219cbfe0083c7631cfb3b09f35a30c94898ffc
rahul-7906 <rahulmelkani4281@gmail.com>
2026-07-02 09:11:30 +0530

    Add variable declaration examples using var, let, const

---
+++ 02-variable declaration keywords.js
@@ -0,0 +1,18 @@
+// var,let,const
+
+//var
+//can be redeclared and updated
+var name = "Rahul";
+var name = "Raju";
+console.log(name);
+
+//let
+//can't redeclare but can update
+let _name= "Rahul";
+_name = "Raju";
+console.log(_name);
+
+//const
+//can't redeclare or update
+const fullName = "Rahul Melkani"
+console.log(fullName);
