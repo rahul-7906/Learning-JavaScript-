4625b3083652b0f4834a5e703e3f0965b9e92184
rahul-7906 <rahulmelkani4281@gmail.com>
2026-07-02 14:14:00 +0530

    Add student object with properties and console logs

---
+++ 03-object.js
@@ -0,0 +1,13 @@
+//objects
+
+const student = {
+  fullName : "Rahul Melkani",
+  age : 17,
+  cgpa : 9.8,
+  isPass : true,
+};
+console.log(student);
+console.log(student.age);
+//other method 
+console.log(student["age"]);
+console.log(student.cgpa);
