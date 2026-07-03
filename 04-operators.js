0a2df57affc2557afd1abbf3456aabd23cc6f7d6
rahul-7906 <rahulmelkani4281@gmail.com>
2026-07-03 16:31:39 +0530

    Add basic arithmetic and comparison operators examples

---
+++ 04-operators.js
@@ -0,0 +1,17 @@
+// operators
+
+// arithematic operators
+let a=5;
+let b=6;
+console.log(a+b);
+console.log(a-b);
+console.log(a**b);
+
+//Comparison operaror
+console.log(a==b);
+console.log(a!=b);
+
+let c = "5";
+console.log(a==c);
+console.log(a===c);
+
