cf2507498189d74fc3c697352eb6ef655b145d08
rahul-7906 <rahulmelkani4281@gmail.com>
2026-07-04 09:11:10 +0530

    Implement for loop for printing and summing numbers
    Added a for loop to print 'Hello World' and calculate the sum of numbers from 1 to 5.

---
+++ 07-for loop.js
@@ -0,0 +1,11 @@
+// for loop
+for(let i =1;i<=5;i++){
+  console.log("Hello World");
+}
+
+// sum using for loop
+let sum = 0;
+for(let i =1; i<= 5;i++){
+  sum = sum + i;
+}
+console.log("sum is",sum);
