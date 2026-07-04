027d281ff0c705d1e87e347554f7052aaffe7cfc
rahul-7906 <rahulmelkani4281@gmail.com>
2026-07-04 09:19:28 +0530

    Implement while and do-while loop examples
    Add while and do-while loop examples in 08-while loop.js

---
+++ 08-while loop.js
@@ -0,0 +1,13 @@
+// while loop
+let i =1;
+while (i<=5){
+  console.log("I am learning javascript");
+  i++;
+}
+
+// do while loop
+let j = 20;
+do{
+  console.log("Hello World");
+  j++;
+} while(j<=10);
