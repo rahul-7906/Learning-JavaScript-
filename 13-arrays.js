de118c77a2fe8ab08067a429ca6868ad1d0e6a46
rahul-7906 <rahulmelkani4281@gmail.com>
2026-07-05 13:38:54 +0530

    Add array example with iteration methods
    This file demonstrates array creation and iteration using for and for-of loops.

---
+++ 13-arrays.js
@@ -0,0 +1,18 @@
+// Arrays 
+let marks = [96,97,98,99,95];
+console.log(marks);
+console.log(marks.length);
+console.log(marks[0]);
+
+// for loop in Arrays
+for(let idx=0;idx < marks.length;idx++){
+  console.log(marks[idx]);
+}
+
+// for of loop in Arrays
+for(let mark of marks){
+  console.log(mark);
+}
+
+
+    
