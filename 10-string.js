0290b66dc9d6af0b6acbe22f6b140e58ec471e68
rahul-7906 <rahulmelkani4281@gmail.com>
2026-07-05 11:53:55 +0530

    Add string manipulation examples in 10-string.js

---
+++ 10-string.js
@@ -0,0 +1,13 @@
+let str = "JavaScript";
+console.log(str);
+console.log(str.length);
+console.log(str[0]);
+
+let str1 = "RahulMelkani";
+console.log(str1);
+console.log(str1.length);
+console.log(str1[0]);
+console.log(str1[1]);
+console.log(str1[2]);
+console.log(str1[3]);
+console.log(str1[4]);
