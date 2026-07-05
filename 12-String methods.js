06bfc505b261c0488f3e6e0c0facf2a7b6ad7d6c
rahul-7906 <rahulmelkani4281@gmail.com>
2026-07-05 12:26:54 +0530

    Implement string method examples in 12-String methods.js
    Add examples of various string methods in JavaScript.

---
+++ 12-String methods.js
@@ -0,0 +1,14 @@
+//String methods
+str = "JavaScript  ";
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.trim());
+console.log(str.slice(0,4));
+console.log(str.slice(3));
+console.log(str.charAt(3));
+
+str2 = "Java";
+console.log(str.concat(str2));
+console.log(str+str2);
+console.log(str.replace(str,str2));
+console.log(str.replace("a","b"));
