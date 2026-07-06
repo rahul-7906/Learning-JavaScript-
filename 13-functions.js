692990ae723e1462d9c0ec2abc1e6c63536f8954
rahul-7906 <rahulmelkani4281@gmail.com>
2026-07-06 06:36:30 +0530

    Add functions for logging messages in JavaScript

---
+++ 13-functions.js
@@ -0,0 +1,18 @@
+//Functions in JavaScript
+
+//Function definition
+function myFunction() {
+  console.log("Hello Everyone!");
+  console.log("I am learning JavaScript");
+}
+//Function Call
+myFunction();
+
+
+//Defining another function
+function newFunction(msg){
+  //parameter -> input
+  console.log(msg);
+}
+newFunction();
+newFunction("I love JS");
